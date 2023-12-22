7233405ed00c28547ec1da89db38dffce7c3796e
Arnaud Lienard <lienard.a@me.com>
2023-12-22 08:59:31 +0100

    modif css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const BestMovie = "http://127.0.0.1:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=";
 
 
-
 async function get(url) {
     try {
         const response = await fetch(url);
